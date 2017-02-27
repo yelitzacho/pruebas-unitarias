9626368ec928115b3f3268043e50c2d9b35c6a28
Yelitza Cho <yelitza.choque@gmail.com>
2017-02-27 12:36:36 -0500

    Pruebas unitarias

---
+++ ejercicio-01/ejercicio-01.js
@@ -0,0 +1,9 @@
+function suma (sum1,sum2) {
+  //Escribe tu codigo aqui
+  return sum1 + sum2;
+}
+
+//NO TOCAR ESTE CODIGO O EXPLOTARA LA PC EN 10 SEGUNDOS
+if(typeof exports !== 'undefined') {
+    exports.suma = suma;
+}
